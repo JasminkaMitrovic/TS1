02dbf8bdfcbf30427169b2c310032be2fbd1250c
Jasminka Mitrovic <jasminka.mitrovic@sage.com>
2019-01-03 11:07:57 +0000

    Added Application list component

---
+++ force-app/main/default/aura/applicationItem/applicationItemController.js
@@ -0,0 +1,12 @@
+({
+    doInit : function(component, event, helper) {
+        /* Nothing to init at the moment, but you can add
+         * number of days untill the loan expires for example...*/
+        /*
+        var mydate = component.get("v.expense.Date__c");
+        if(mydate){
+            component.set("v.formatdate", new Date(mydate));
+        }
+        */
+    },
+})
